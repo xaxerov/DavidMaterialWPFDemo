980923891891bf5c63f9868afec32dc94dd669c5
Xax <xaxerov@gmail.com>
2021-03-11 15:52:50 +0400

    Add project files.

---
+++ DavidMaterialWPFDemo/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using DavidMaterialWPFDemo.ViewModels;
+using DavidMaterialWPFDemo.Views;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DavidMaterialWPFDemo
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand RunExtendedDialogCommand => new AnotherCommandImplementation(ExecuteRunExtendedDialog);
+        public MainViewModel()
+        {
+        }
+        private async void ExecuteRunExtendedDialog(object o)
+        {
+            //let's set up a little MVVM, cos that's what the cool kids are doing:
+            var view = new SampleDialog()
+            {
+                DataContext = new SampleDialogViewModel()
+            };
+
+            //show the dialog
+            var result = await DialogHost.Show(view, "RootDialog", ExtendedOpenedEventHandler, ExtendedClosingEventHandler);
+
+            //check the result...
+            Debug.WriteLine("Dialog was closed, the CommandParameter used to close it was: " + (result ?? "NULL"));
+        }
+
+        private void ExtendedOpenedEventHandler(object sender, DialogOpenedEventArgs eventargs)
+            => Debug.WriteLine("You could intercept the open and affect the dialog using eventArgs.Session.");
+
+        private void ExtendedClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
+        {
+            if (eventArgs.Parameter is bool parameter &&
+                parameter == false) return;
+
+            //OK, lets cancel the close...
+            eventArgs.Cancel();
+
+            var progressViewModel = new SampleProgressDialogViewModel();
+            var progressView = new SampleProgressDialog() { DataContext = progressViewModel };
+            //...now, lets update the "session" with some new content!
+            eventArgs.Session.UpdateContent(progressView);
+            //note, you can also grab the session when the dialog opens via the DialogOpenedEventHandler
+
+            //lets run a fake operation for 3 seconds then close this baby.
+            progressViewModel.Execute()
+                .ContinueWith((t, _) => eventArgs.Session.Close(false), null,
+                    TaskScheduler.FromCurrentSynchronizationContext());
+        }
+    }
+}
---
+++ DavidMaterialWPFDemo/ViewModels/SampleProgressDialogViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DavidMaterialWPFDemo.ViewModels
+{
+    public class SampleProgressDialogViewModel : ViewModelBase
+    {
+        public SampleProgressDialogViewModel()
+        {
+            _progress = 0;
+            Progress = 0;
+        }
+
+        public bool Execute()
+        {
+            bool success = false;
+            while (Progress <= 100)
+            {
+                Thread.Sleep(100);
+                ++Progress;
+            }
+            success = true;
+            return success;
+        }
+
+        private double _progress;
+        public double Progress
+        {
+            get => _progress;
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    SetProperty(ref _progress, value);
+            }
+        }
+    }
+}
